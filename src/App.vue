b77333bd9a5a701d76c533d0a048706dc6bc98a7
Alex <Sersurgut88@yandex.ru>
2020-12-18 15:43:59 +0300

    start app and create store

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app"></div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line prettier/prettier
+  name: "App",
+};
+</script>
+
+<style>
+#app {
+}
+</style>
